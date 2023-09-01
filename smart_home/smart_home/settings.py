deedbf95bef4c0f1dee10ac86c9915f57c66d062
GVladislav <grishinvladislav97@gmail.com>
2023-09-01 16:21:22 +0300

    fix conf

--- smart_home/smart_home/settings.py
+++ smart_home/smart_home/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'PASSWORD': os.get_env('DB_PASS')
+        'PASSWORD': os.getenv('DB_PASS')
     }
 }
 
